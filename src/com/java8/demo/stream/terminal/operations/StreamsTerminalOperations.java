2d62e58ab37891bb3491d60bf41b085bb6ac5fc5
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-14 01:38:02 +0530

    14/01/2024:01:37:00AM

--- src/com/java8/demo/stream/terminal/operations/StreamsTerminalOperations.java
+++ src/com/java8/demo/stream/terminal/operations/StreamsTerminalOperations.java
@@ -1,14 +1,43 @@
 package com.java8.demo.stream.terminal.operations;
 
+import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class StreamsTerminalOperations {
-
+public class StreamsTerminalOperations {	
+	
+    /*
+     * 
+     * Java-8 Stream terminal operations produces a non-stream, result such as primitive value, a collection
+     * or no value at all. Terminal operations are typically preceded by intermediate operations which return
+     * another Stream which allows operations to be connected in a form of query.
+     * 
+     * 1.) The terminal operations of the Java Stream interface typically return a single value.
+     * 2.) Terminal operations can't be chained together.
+     * 3.) Intermediate operations return another stream as a result, they can be chained together to form a
+     *     pipeline of operations.
+     *     
+     * Terminal Stream Operations:
+     * 
+     * anyMatch()
+     * allMatch()
+     * noneMatch()
+     * collect()
+     * count()
+     * findAny()
+     * firstAny()
+     * forEach()
+     * min()
+     * max()
+     * reduce()
+     * toArray()   
+     * 
+     */
 	public static void main(String[] args) {
 		
 		List<String> languages = List.of("Java", "Scala", "Rust", "Kotlin", "Go");	
@@ -16,6 +45,9 @@ public class StreamsTerminalOperations {
 		// 1. ForEach
 		languages.stream().map(language -> language.toUpperCase())
 		                  .forEach(language -> System.out.println(language));
+		
+		languages.stream().map(language -> language.toUpperCase())
+                          .forEach(System.out::println);
 		
 		// 2. toList | Map | Set
 		List<String> upperLanguages = languages.stream().map(language -> language.toUpperCase())
@@ -27,7 +59,7 @@ public class StreamsTerminalOperations {
 		System.out.println(anyMatch);
 		
 		// 4. Collect ( toList | toSet )
-		List<Integer> numbers = List.of(2,5,7,8,9,3,4,6,4,6,1);
+		List<Integer> numbers = List.of(2,5,7,8,9,3,4,6,4,6,1,-3,-234);
 		Set<Integer> unique_numbers = numbers.stream().distinct().sorted().collect(Collectors.toSet());
 		System.out.println(unique_numbers);
 		
@@ -66,6 +98,30 @@ public class StreamsTerminalOperations {
 		
 		Map<Boolean, List<Integer>> partitionNumber = numbers.stream().collect(Collectors.partitioningBy(number -> number % 2 == 0));
 	    System.out.println(partitionNumber);
+	    
+	    // 11. GroupingBy
+	    Map<String, String> users = Map.of("Varun", "USER", "Karun", "USER", "Tarun", "ADMIN", "Jeevan", "SUPER_USER", "Sam", "SUPER_USER", "Tarkan", "USER");
+	    Map<String, List<Entry<String, String>>> group_by_users = users.entrySet().stream().collect(Collectors.groupingBy(Map.Entry::getValue));
+	    System.out.println(group_by_users);
+	    
+	    // 12. Reduce
+	    Integer reduce = numbers.stream().reduce(0, Integer::min);
+	    System.out.println(reduce);
+	    Integer listOfSum = numbers.stream().reduce(0, Integer::sum);
+	    System.out.println(listOfSum);
+	    
+	    // 13. toArray
+	    Object[] array = languages.stream().toArray();
+	    System.out.println(Arrays.toString(array));
+	    
+	    // 14. findAny
+	    Optional<String> findAny = languages.stream().findAny();
+	    System.out.println(findAny.get());
+	    
+	    // 15. findFirst
+	    Optional<String> findFirst = languages.stream().findFirst();
+	    System.out.println(findFirst.get());
+	    
 	}
 
 }
