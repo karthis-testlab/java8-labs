0f68946de98fc0c035738d55ecc798d16f4a97be
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-01-21 00:19:32 +0530

    21/01/2024:12:19:00AM

---
+++ src/com/java8/interview/qa/string/FindDuplicateInString.java
@@ -0,0 +1,31 @@
+package com.java8.interview.qa.string;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindDuplicateInString {
+
+	// Find all duplicate elements from a give string
+	public static void main(String[] args) {
+		
+		String str = "all duplicate elements";
+		String[] stringArray = str.replace(" ", "").split("");
+		
+		List<String> collect = Arrays.stream(stringArray)
+	      .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+	      .entrySet().stream()
+	      .filter(character -> character.getValue() > 1)
+	      .map(Map.Entry::getKey)
+	      .collect(Collectors.toList());
+		
+		System.out.println(collect);
+		
+		
+		
+	}
+
+}
