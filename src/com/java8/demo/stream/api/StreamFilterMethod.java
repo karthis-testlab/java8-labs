5dab3906f4a9b55fa2bde0773c2c4bdb395b06d4
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-05-07 00:01:14 +0530

    07/05/2024:12:01AM

--- src/com/java8/demo/stream/api/StreamFilterMethod.java
+++ src/com/java8/demo/stream/api/StreamFilterMethod.java
@@ -7,7 +7,7 @@ public class StreamFilterMethod {
 
 	public static void main(String[] args) {
 		
-		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12);
 		
 		final List<Integer> output = numbers.stream()
 				.filter(x -> {
