76277c5c6d295402af85632e29b16bb5543383bb
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-16 00:08:25 +0530

    16/10/2024:12:08:00AM

--- src/com/java8/demo/stream/api/ShortByValues.java
+++ src/com/java8/demo/stream/api/ShortByValues.java
@@ -1,10 +1,19 @@
 package com.java8.demo.stream.api;
 
+import java.util.Map;
+
 public class ShortByValues {
 
 	public static void main(String[] args) {
 		
+		var map = Map.of("Java", 85, "Rust", 79, "Go", 83, "Kotlin", 70, "Groovy", 60);
+		Map<String, Integer> sortingByValues = sortingByValues(map);
+		System.out.println(sortingByValues);
 
+	}
+	
+	public static Map<String, Integer> sortingByValues(Map<String, Integer> map) {
+		return map;		
 	}
 
 }
