e08855e02f6e2bc3eb5a02f68fdfa3c065c76ae9
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-17 00:08:50 +0530

    17/10/2024:12:08:00AM

--- src/com/java8/demo/stream/api/ShortByValues.java
+++ src/com/java8/demo/stream/api/ShortByValues.java
@@ -1,6 +1,8 @@
 package com.java8.demo.stream.api;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ShortByValues {
 
@@ -13,7 +15,12 @@ public class ShortByValues {
 	}
 	
 	public static Map<String, Integer> sortingByValues(Map<String, Integer> map) {
-		return map;		
+		return map.entrySet()
+				  .stream()
+				  .sorted(Map.Entry.comparingByValue())
+				  .collect(Collectors.toMap(Map.Entry::getKey,
+						                    Map.Entry::getValue,
+						                    (e1, e2) -> e1, LinkedHashMap::new));
 	}
 
 }
