40507d0953b82f08a21ce00b99cb57348c200c05
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-25 23:38:39 +0530

    25/10/2024:11:38:00PM

--- src/com/java8/demo/stream/api/SplitIntoSubLists.java
+++ src/com/java8/demo/stream/api/SplitIntoSubLists.java
@@ -1,9 +1,12 @@
 package com.java8.demo.stream.api;
 
+import java.util.Arrays;
+
 public class SplitIntoSubLists {
 
 	public static void main(String[] args) {
-		
+		var list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		int k = 4;
 	}
 
 }
