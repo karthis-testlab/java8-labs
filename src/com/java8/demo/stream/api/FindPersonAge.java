4af151ca10824a505efda0dbb6ee1a764aeda4df
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-10-30 00:10:13 +0530

    30/10/2024:12:10:00AM

--- src/com/java8/demo/stream/api/FindPersonAge.java
+++ src/com/java8/demo/stream/api/FindPersonAge.java
@@ -1,9 +1,21 @@
 package com.java8.demo.stream.api;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
 public class FindPersonAge {
 
 	public static void main(String[] args) {
-		
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter your brithdate (YYYY-MM-DD): ");
+			String birthDateString = scanner.nextLine();
+			LocalDate birthDate = LocalDate.parse(birthDateString);
+			// Get the current date
+			LocalDate currentDate = LocalDate.now();
+			Period age = Period.between(birthDate, currentDate);
+			System.out.println("You are "+ age.getYears()+ " years old.");
+		}
 	}
 
 }
