a884b09b0089a0e04c7d272119b1bba97694d224
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-12 23:54:43 +0530

    11/11/2024:11:54:00PM

--- src/com/java8/interview/qa/integer/FindAverage.java
+++ src/com/java8/interview/qa/integer/FindAverage.java
@@ -2,8 +2,7 @@ package com.java8.interview.qa.integer;
 
 public class FindAverage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 	}
 
