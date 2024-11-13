15b4558dc0afb7348b8871f634c9e91ecd1eeb76
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-13 19:59:06 +0530

    13/11/2024:07:59:00PM

--- src/com/java8/interview/qa/integer/FindAverage.java
+++ src/com/java8/interview/qa/integer/FindAverage.java
@@ -1,8 +1,13 @@
 package com.java8.interview.qa.integer;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FindAverage {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
+		List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
 
 	}
 
