ce74281650f3b27ba838267b2d2e8a617e12aa8d
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-18 23:31:38 +0530

    18/11/2024:11:31:00PM

--- src/com/java8/interview/qa/string/StringCase.java
+++ src/com/java8/interview/qa/string/StringCase.java
@@ -2,7 +2,9 @@ package com.java8.interview.qa.string;
 
 public class StringCase {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		
+		//Given input
 
 	}
 
