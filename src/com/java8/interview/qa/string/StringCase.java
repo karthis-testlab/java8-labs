b93ab12a14414631420f67bd8d945677a5d5af0f
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2024-11-19 23:54:15 +0530

    19/11/2024:11:54:00PM

--- src/com/java8/interview/qa/string/StringCase.java
+++ src/com/java8/interview/qa/string/StringCase.java
@@ -1,11 +1,16 @@
 package com.java8.interview.qa.string;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCase {
 
 	public static void main(String[] args) {
 		
 		//Given input
+		List<String> words = Arrays.asList("Apple", "Banana", "Mango", "cherry", "Orange");
 
+		
 	}
 
 }
