de671279238e80853c98234a2011cdd639231abf
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2025-01-28 00:44:16 +0530

    28/01/2025:12:44:00AM

--- src/com/java8/demo/stream/api/FindIndexBasedonElement.java
+++ src/com/java8/demo/stream/api/FindIndexBasedonElement.java
@@ -1,11 +1,22 @@
 package com.java8.demo.stream.api;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class FindIndexBasedonElement {
 
 	public static void main(String[] args) {
-		
-		
 
+		List<String> fruits = Arrays.asList("apple", "banana", "cherry", "dates", "cheery");
+        
+		int index = IntStream.range(0, fruits.size())
+		         .filter(i -> fruits.get(i).equals("cherry"))
+		         .findFirst()
+		         .getAsInt();
+		
+		System.out.println(index);
+		
 	}
 
 }
